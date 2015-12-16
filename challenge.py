f7d05efc6999a4a10644cca59153345086f0312a
Dan Conley <dconley@synacor.com>
2015-12-16 11:46:13 -0500

    It works! Hooray!

--- challenge.py
+++ challenge.py
@@ -59,7 +59,6 @@ class SynChallenge:
 
 	def jmp(self, index):
 		self.setindex(index)
-		self.parse()
 
 	def setregister(self, index, value):
 		self.register[index] = value
@@ -71,110 +70,117 @@ class SynChallenge:
 		self.jmp(self.readnext())
 
 	def parse(self):
-		curr = self.readbyte()
-		if (curr == 0): # halt
-			sys.exit()
-		elif (curr == 1): #set
-			self.setregister(self.readnext(False), self.readnext())
-		elif (curr == 2): # push
-			value = self.readnext()
-			self.stack.append(value)
-			self.debug('appended ' + str(value) + ' to stack (stack is ' + str(self.stack) + ')')
-		elif (curr == 3): #pop
-			index = self.readnext('raw')
-			value = self.stack.pop()
-			self.writebyte(index, value)
-			self.debug('popped ' + str(value) + ' off the stack and wrote it to byte ' + str(index) + ' (stack is ' + str(self.stack) + ')')
-		elif (curr == 4): # eq
-			index = self.readnext('raw')
-			a = self.readnext()
-			b = self.readnext()
-			if (a == b):
-				self.writebyte(index, 1)
-			else:
-				self.debug(str(a) + ' is not equal to ' + str(b))
-				self.writebyte(index, 0)
-		elif (curr == 5): # gt
-			index = self.readnext('raw')
-			if (self.readnext() > self.readnext()):
-				self.writebyte(index, 1)
-			else:
-				self.writebyte(index, 0)
-		elif (curr == 6): # jmp
-			self.jmp(self.readnext())
-		elif (curr == 7): # jt
-			value = self.readnext()
-			if (value != 0):
-				self.debug(str(self.index) + ' is ' + str(value) + ', not 0, jmping')
+		while True:
+			curr = self.readbyte()
+			if (curr == 0): # halt
+				sys.exit()
+			elif (curr == 1): #set
+				self.setregister(self.readnext(False), self.readnext())
+			elif (curr == 2): # push
+				value = self.readnext()
+				self.stack.append(value)
+				self.debug('appended ' + str(value) + ' to stack (stack is ' + str(self.stack) + ')')
+			elif (curr == 3): #pop
+				index = self.readnext('raw')
+				value = self.stack.pop()
+				self.writebyte(index, value)
+				self.debug('popped ' + str(value) + ' off the stack and wrote it to byte ' + str(index) + ' (stack is ' + str(self.stack) + ')')
+			elif (curr == 4): # eq
+				index = self.readnext('raw')
+				a = self.readnext()
+				b = self.readnext()
+				if (a == b):
+					self.writebyte(index, 1)
+				else:
+					self.debug(str(a) + ' is not equal to ' + str(b))
+					self.writebyte(index, 0)
+			elif (curr == 5): # gt
+				index = self.readnext('raw')
+				if (self.readnext() > self.readnext()):
+					self.writebyte(index, 1)
+				else:
+					self.writebyte(index, 0)
+			elif (curr == 6): # jmp
 				self.jmp(self.readnext())
+				continue
+			elif (curr == 7): # jt
+				value = self.readnext()
+				if (value != 0):
+					self.debug(str(self.index) + ' is ' + str(value) + ', not 0, jmping')
+					self.jmp(self.readnext())
+					continue
+				else:
+					self.debug(str(self.index) + ' is 0, no jmp')
+					self.setindex(self.index + 1)
+			elif (curr == 8): # jf
+				value = self.readnext()
+				if (value == 0):
+					self.debug(str(self.index) + ' is 0, jmping')
+					self.jmp(self.readnext())
+					continue
+				else:
+					self.debug(str(self.index) + ' is ' + str(value) + ', not 0, no jmp')
+					self.index += 1
+			elif (curr == 9): # add
+				index = self.readnext('raw')
+				value = (self.readnext() + self.readnext()) % 32768
+				self.writebyte(index, value)
+			elif (curr == 10): # mult
+				index = self.readnext('raw')
+				value = (self.readnext() * self.readnext()) % 32768
+				# so, fun times: if you accidentally multiply three numbers above it skips to the next part without
+				# actually doing what it should and triggers an easter egg!
+				# I discovered this because I am super clever and not at all because I misread the spec
+				self.writebyte(index, value)
+			elif (curr == 11): # mod
+				index = self.readnext('raw')
+				value = (self.readnext() % self.readnext()) % 32768
+				self.writebyte(index, value)
+			elif (curr == 12): # and
+				index = self.readnext('raw')
+				self.writebyte(index, self.readnext() & self.readnext() % 32768)
+			elif (curr == 13): # or
+				index = self.readnext('raw')
+				self.writebyte(index, self.readnext() | self.readnext() % 32768)
+			elif (curr == 14): # not
+				index = self.readnext('raw')
+				value = self.readnext()
+				notval = (~value & 0xFFFF) % 32768 # mgeneral gave me this
+				self.debug('bitwise not of ' + str(value) + ' is ' + str(notval))
+				self.writebyte(index, notval)
+			elif (curr == 15): # rmem
+				index = self.readnext(False)
+				tmp = self.index + 1
+				self.setindex(self.readnext())
+				value = self.readbyte()
+				self.setindex(tmp)
+				self.debug('rmem with register ' + str(index) + ' and value ' + str(value))
+				self.setregister(index, value)
+			elif (curr == 16): # wmem
+				index = self.readnext()
+				value = self.readnext()
+				self.debug('wmem with index ' + str(index) + ' and value ' + str(value))
+				self.writebyte(index, value)
+			elif (curr == 17): # call
+				self.call()
+				continue
+			elif (curr == 18): # ret
+				index = self.stack.pop()
+				self.debug('ret returned ' + str(index))
+				self.jmp(index)
+				continue
+			elif (curr == 19): # out
+				if (self.isdebug() == False):
+					sys.stdout.write(chr(self.readnext()))
+				else:
+					self.setindex(self.index + 1)
+			elif (curr == 20): # in
+				self.writebyte(self.readnext('raw'), ord(sys.stdin.read(1)))
+			elif (curr == 21): # noop
+				pass
 			else:
-				self.debug(str(self.index) + ' is 0, no jmp')
-				self.setindex(self.index + 1)
-		elif (curr == 8): # jf
-			value = self.readnext()
-			if (value == 0):
-				self.debug(str(self.index) + ' is 0, jmping')
-				self.jmp(self.readnext())
-			else:
-				self.debug(str(self.index) + ' is ' + str(value) + ', not 0, no jmp')
-				self.index += 1
-		elif (curr == 9): # add
-			index = self.readnext('raw')
-			value = (self.readnext() + self.readnext()) % 32768
-			self.writebyte(index, value)
-		elif (curr == 10): # mult
-			index = self.readnext('raw')
-			value = (self.readnext() * self.readnext()) % 32768
-			# so, fun times: if you accidentally multiply three numbers above it skips to the next part without
-			# actually doing what it should and triggers an easter egg!
-			# I discovered this because I am super clever and not at all because I misread the spec
-			self.writebyte(index, value)
-		elif (curr == 11): # mod
-			index = self.readnext('raw')
-			value = (self.readnext() % self.readnext()) % 32768
-			self.writebyte(index, value)
-		elif (curr == 12): # and
-			index = self.readnext('raw')
-			self.writebyte(index, self.readnext() & self.readnext() % 32768)
-		elif (curr == 13): # or
-			index = self.readnext('raw')
-			self.writebyte(index, self.readnext() | self.readnext() % 32768)
-		elif (curr == 14): # not
-			index = self.readnext('raw')
-			value = self.readnext()
-			notval = (~value & 0xFFFF) % 32768 # mgeneral gave me this
-			self.debug('bitwise not of ' + str(value) + ' is ' + str(notval))
-			self.writebyte(index, notval)
-		elif (curr == 15): # rmem
-			index = self.readnext(False)
-			tmp = self.index + 1
-			self.setindex(self.readnext())
-			value = self.readbyte()
-			self.setindex(tmp)
-			self.debug('rmem with register ' + str(index) + ' and value ' + str(value))
-			self.setregister(index, value)
-		elif (curr == 16): # wmem
-			index = self.readnext()
-			value = self.readnext()
-			self.debug('wmem with index ' + str(index) + ' and value ' + str(value))
-			self.writebyte(index, value)
-		elif (curr == 17): # call
-			self.call()
-		elif (curr == 18): # ret
-			index = self.stack.pop()
-			self.debug('ret returned ' + str(index))
-			self.jmp(index)
-		elif (curr == 19): # out
-			if (self.isdebug() == False):
-				print chr(self.readnext()),
-			else:
-				self.setindex(self.index + 1)
-		elif (curr == 21): # noop
-			pass
-		else:
-			sys.exit(str(curr) + ' is not configured')
-		self.setindex(self.index + 1)
-		self.parse()
+				sys.exit(str(curr) + ' is not configured')
+			self.setindex(self.index + 1)
 
 challenge = SynChallenge()
 challenge.parse()
